83bcf4c68fc37173bd13995557bfb1c465252492
Bertrand Le roy <bertrandleroy@gmail.com>
2021-08-30 00:01:11 -0700

    Compute angles from coordinates
    Add a test section to the page
    Write tests
    Add documentation

--- Jaggilines/default.js
+++ Jaggilines/default.js
@@ -50,13 +50,16 @@ const angleUnitPowerOfTwo = 2;
 const north = 90 << angleUnitPowerOfTwo;
 
 /** East direction in units of angle. */
-const east = 0 << angleUnitPowerOfTwo;
+const east = 0;
 
 /** West direction in units of angle. */
 const west = 180 << angleUnitPowerOfTwo;
 
 /** South direction in units of angle. */
 const south = 270 << angleUnitPowerOfTwo;
+
+/** Number of units of angle in a quarter of a circle. */
+const quarterCircle = 90 << angleUnitPowerOfTwo;
 
 /** Number of units of angle in a half circle. */
 const halfCircle = 180 << angleUnitPowerOfTwo;
@@ -92,7 +95,7 @@ const clamp = (val, max = maxHeight) => val < 0 ? 0 : val > max ? max : Math.flo
  * @param {number} val - The value to vary around.
  * @param {number} varyBy - The maximum variation around val.
  * @returns {number} A new integer value that is a random number equal to val +/- at most varyBy and clamped between 0 and maxHeight. */
-const vary = (val, varyBy = maxVariation) => clamp(val + Math.random() * 2 * varyBy - varyBy);
+const vary = (val, varyBy = maxVariation, max = maxHeight) => clamp(val + Math.random() * 2 * varyBy - varyBy, max);
 
 /** Adds a random variation around the average of the provided values, then clamps the result.
  * @param {number} val - The value to vary around.
@@ -101,10 +104,20 @@ const vary = (val, varyBy = maxVariation) => clamp(val + Math.random() * 2 * var
  const varyFromAverage = (vals, varyBy = maxVariation) => vary(vals.reduce((acc, val) => acc + val, 0) / vals.length, varyBy);
 
 /** A table of twelve bits of cos for each unit of angle. */
-const cos = [...new Array(fullCircle).keys()].map(angle => Math.floor(Math.cos(angle * Math.PI / halfCircle) * (1 << bitsBetweenTops)));
+const cosTable = [...new Array(fullCircle).keys()].map(angle => Math.floor(Math.cos(angle * Math.PI / halfCircle) * (1 << bitsBetweenTops)));
+
+/** The cosinus of an angle in local units, expressed as a signed 12-bit integer.
+ * @param {number} angle - The angle in local units of angle.
+ * @returns {number} the cosinus of the angle as a signed 12-bit integer. */
+const cos = angle => cosTable[mod(angle, fullCircle)];
 
 /** A table of twelve bits of sin for each unit of angle. */
-const sin = [...new Array(fullCircle).keys()].map(angle => Math.floor(Math.sin(angle * Math.PI / halfCircle) * (1 << bitsBetweenTops)));
+const sinTable = [...new Array(fullCircle).keys()].map(angle => Math.floor(Math.sin(angle * Math.PI / halfCircle) * (1 << bitsBetweenTops)));
+
+/** The sinus of an angle in local units, expressed as a signed 12-bit integer.
+ * @param {number} angle - The angle in local units of angle.
+ * @returns {number} the sinus of the angle as a signed 12-bit integer. */
+ const sin = angle => sinTable[mod(angle, fullCircle)];
 
 /** A table of fixed-point 16 bits of tangents for each unit of angle between west and north (north excluded, since tan is infinity there).
  * The fractional point is at 8 bits: [8 bits 15-8 integral part][8 bits 7-0 fractional part]. */
@@ -114,14 +127,26 @@ const sin = [...new Array(fullCircle).keys()].map(angle => Math.floor(Math.sin(a
 // Entries are then between 0 and 56871, requiring 16 bits to encode.
 // Example: tan(45deg) = 1. 45 degrees are encoded as 180 (45 << 2), so the entry for index 180 should be close to the fixed-point
 // value for 1, which is 1 << 8 = 256.
-const tan = [...new Array(fullCircle >> 2).keys()].map(angle => Math.round(Math.tan(angle * Math.PI / halfCircle) * (1 << 8)));
-console.log(tan);
+const tanTable = [...new Array(fullCircle >> 2).keys()].map(angle => Math.round(Math.tan(angle * Math.PI / halfCircle) * (1 << 8)));
+
+/** The tangent of an angle in local units, expressed as a signed 16-bit integer.
+ * @param {number} angle - The angle in local units of angle.
+ * @returns {number} the sinus of the angle as a signed 12-bit integer. */
+const tan = angle => {
+    if ((angle - quarterCircle) % halfCircle === 0) {
+        return undefined;
+    }
+    const clampedAngle = mod(angle, halfCircle); // The period of tan is π/2
+    const tableIndex = (clampedAngle > quarterCircle ? halfCircle - clampedAngle : clampedAngle);
+    const absResult = tanTable[tableIndex];
+    return clampedAngle > quarterCircle ? - absResult : absResult;
+}
 
 /** Maps coordinates to angles
  * @param {number} x - The horizontal coordinate.
  * @param {number} y - The vertical coordinate.
  * @returns {number} The angle corresponding to those coordinates. */
-const atan = (x, y) => {
+const angleFromCoordinates = (x, y) => {
     if (x == 0) return (y > 0) ? north : south;
     // We'll map the negative-x or negative-y quadrants by using the symmetries of the tan function when processing the resulting angle.
     const absX = Math.abs(x);
@@ -134,16 +159,28 @@ const atan = (x, y) => {
     // and then interpolate the rest. Since we're not as resource-constrained as coders of the time, we can leave this as an exercise
     // to the reader and simplify the code, knowing that this optimization would be possible.
     const ratio = Math.round((absY << 8) / absX); // The result is a 16 bit integer: |y| / |x| <= 1 << 8 so |y| << 8 / |x| <= 1 << 16
-    // ... binary search this ratio in the tan table
-    // ... use x and y signs and tan symmetries to determine the angle
+    const angle = findIndexOf(ratio, tanTable);
+    return x > 0 ?
+        y > 0 ? angle : fullCircle - angle :
+        y > 0 ? halfCircle - angle : halfCircle + angle;
 };
 
 /** Find the index of the value in the table that's closest to the provided value.
  * @param {number} val - The value to search for.
- * @param {number[]} table - The table in which to search.
- * @returns {number} The index in the table of the closest value. */
-const findIndex = (val, table) => {
-    // ...
+ * @param {number[]} table - The table in which to search. Values in the table must be in order.
+ * @returns {number} The index of the closest value in the table. */
+const findIndexOf = (val, table) => {
+    let low = 0;
+    let high = table.length - 1
+    while (true) {
+        if (high - low === 1) {
+            return table[high] <= val ? high : low;
+        }
+        const mid = (low + high) >> 1;
+        if (table[mid] === val) return mid;
+        if (val < table[mid]) high = mid;
+        else low = mid;
+    }
 };
 
 /** Positive modulo.
@@ -474,19 +511,195 @@ class Valkyrie {
     move() {
         // TODO: handle mountain collision and top altitude
         this.heading = mod(this.heading + this.roll * this.rollToAngle, fullCircle);
-        this.x = mod(this.x + ((this.thrust * cos[this.heading] * cos[this.pitch]) >> bitsBetweenTops >> bitsBetweenTops), 1 << coordinateBits);
-        this.y = mod(this.y - ((this.thrust * sin[this.heading] * cos[this.pitch]) >> bitsBetweenTops >> bitsBetweenTops), 1 << coordinateBits);
-        this.z = clamp(this.z + (this.thrust * sin[this.pitch]) >> bitsBetweenTops, 1 << coordinateBits);
+        this.x = mod(this.x + ((this.thrust * cosTable[this.heading] * cosTable[this.pitch]) >> bitsBetweenTops >> bitsBetweenTops), 1 << coordinateBits);
+        this.y = mod(this.y - ((this.thrust * sinTable[this.heading] * cosTable[this.pitch]) >> bitsBetweenTops >> bitsBetweenTops), 1 << coordinateBits);
+        this.z = clamp(this.z + (this.thrust * sinTable[this.pitch]) >> bitsBetweenTops, 1 << coordinateBits);
         this.#moveHandlers.forEach(handler => handler(this));
     }
 }
+
+/** Creates an element of the supplied tag type and with the specified attributes.
+ * @param {HTMLElement} parentEl - A parent element to append the new element to.
+ * @param {string} tag - The tag name.
+ * @param {object} attr - An object containing all the attributes to set on the element.
+ * @returns {HTMLElement} The new element. */
+ const createEl = (parentEl, tag, attr) => {
+    const el = parentEl.ownerDocument.createElement(tag);
+    if (attr) {
+        for (let option in attr) {
+            if (option === '_') el.innerHTML = attr[option];
+            else el[option] = attr[option];
+        }
+    }
+    parentEl.appendChild(el);
+    return el;
+}
+
+/** Creates a function that appends a graph to the supplied element.
+ * @param {HTMLElement} parentEl - The parent element to which the returned function will append graphs.
+ * @returns {TestGrapher} - A function that can append a graph to the provided parent element. */
+const graphTester = parentEl =>
+    /** Graphs a function.
+     * @callback TestGrapher
+     * @param {Function} fn - The function to graph.
+     * @param {number[]} xRange - A two elements array containing the lower and higher ends of the x range to graph.
+     * @param {number[]} yRange - A two elements array containing the lower and higher ends of the y range to graph.
+     * @param {number} scaleX - The scale to apply on the X axis.
+     * @param {number} scaleY - The scale to apply on the Y axis.
+     * @param {string} title - An optional title for the graph. If not provided, tha name of the function is used. */
+    (fn, xRange, yRange, scaleX, scaleY, title) => {
+        createEl(parentEl, 'h1', {innerText: title || fn.name});
+        const canvas = createEl(parentEl, 'canvas', {
+            width: Math.ceil((xRange[1] - xRange[0]) * scaleX),
+            height: Math.ceil((yRange[1] - yRange[0]) * scaleY)
+        });
+        const ctx = canvas.getContext('2d');
+        const orgX = -xRange[0] * scaleX;
+        const orgY = canvas.height + yRange[0] * scaleY;
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'blue';
+        if (orgX > 0 && orgX < canvas.width) {
+            ctx.moveTo(orgX, 0);
+            ctx.lineTo(orgX, canvas.height);
+        }
+        if (orgY > 0 && orgY < canvas.height) {
+            ctx.moveTo(0, orgY);
+            ctx.lineTo(canvas.width, orgY);
+        }
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        let previousWasDefined = false;
+        for (let x = xRange[0]; x <= xRange[1]; x += 1 / scaleX) {
+            const y = fn(x);
+            const screenX = x * scaleX + orgX;
+            if (isNaN(y)) {
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.strokeStyle = 'red';
+                ctx.moveTo(screenX, 0);
+                ctx.lineTo(screenX, canvas.height);
+                ctx.stroke();
+                ctx.strokeStyle = 'black';
+                previousWasDefined = false;
+                continue;
+            }
+            const screenY = - y * scaleY + orgY;
+            if (previousWasDefined) {
+                ctx.lineTo(screenX, screenY);
+            }
+            else {
+                ctx.moveTo(screenX, screenY);
+            }
+            previousWasDefined = true;
+        }
+        ctx.stroke();
+    };
+
+/** Creates a function that appends a table of expected vs. actual values of a function to the supplied element.
+ * @param {HTMLElement} parentEl - The parent element to which the returned function will append tables.
+ * @returns {RangeTester} - A function that can append a table to the provided parent element. */
+ const rangeTester = parentEl =>
+    /** Tests the values of a function and renders a table of expected vs. actual values.
+     * @callback RangeTester
+     * @param {Function} fn - The function to test.
+     * @param {Array} data - The values to feed the function.
+     * @param {Array} expected - The expected values of the function over the data, in the same order.
+     * @param {string} title - An optional title for the test. If not provided, tha name of the function is used. */
+     (fn, data, expected, title) => {
+        createEl(parentEl, 'h1', {innerText: title || fn.name});
+        const table = createEl(parentEl, 'table');
+        const body = createEl(table, 'tbody');
+        
+        const expectedRow = createEl(body, 'tr');
+        createEl(expectedRow, 'td', {_: '<b>Expected:</b>'});
+        const actualRow = createEl(body, 'tr');
+        createEl(actualRow, 'td', {_: '<b>Actual:</b>'});
+        for (let i = 0; i < data.length; i++) {
+            const actual = fn(data[i]);
+            createEl(expectedRow, 'td', {_: expected[i]});
+            createEl(actualRow, 'td', {
+                _: actual,
+                className: actual === expected[i] ? 'pass' : 'fail'
+            });
+        }
+    };
+
+/** A field value.
+ * @typedef {object} FieldValue
+ * @property {number} angle - The angle for the field value. Can be undefined for a scalar field.
+ * @property {number} amplitude - The amplitude of the field value. May be undefined. */
+
+/** A 2D scalar or vector field.
+ * @callback Field
+ * @param {number} x - The x coordinate.
+ * @param {number} y - The y coordinate.
+ * @returns {FieldValue} The value of the field at the point described by the coordinates. */
+
+/** Creates a function that appends a 2D representation of a field to the supplied element.
+ * @param {HTMLElement} parentEl - The parent element to which the returned function will append the field representation.
+ * @returns {FieldTester} - A function that can append a field representation to the provided parent element. */
+ const fieldTester = parentEl =>
+    /** Tests the values of a function and renders a table of expected vs. actual values.
+     * @callback FieldTester
+     * @param {Field} fn - The field to test.
+     * @param {Array} xRange - The lower and higher bounds of the x coordinate for the field representation.
+     * @param {Array} yRange - The lower and higher bounds of the y coordinate for the field representation.
+     * @param {number} scale - The scaling factor for the coordinates of the field representation on the screen.
+     * @param {number} step - The step for the incrementation of the x and y coordinates, or the distance between the representations of two adjacent field values.
+     * @param {string} title - An optional title for the test. If not provided, tha name of the function is used. */
+     (fn, xRange, yRange, scale, step, title) =>{
+        createEl(parentEl, 'h1', {innerText: title || fn.name});
+        const canvas = createEl(parentEl, 'canvas', {
+            width: Math.ceil((xRange[1] - xRange[0]) * scale),
+            height: Math.ceil((yRange[1] - yRange[0]) * scale)
+        });
+        const ctx = canvas.getContext('2d');
+        const orgX = -xRange[0] * scale;
+        const orgY = canvas.height + yRange[0] * scale;
+        ctx.beginPath();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'blue';
+        if (orgX > 0 && orgX < canvas.width) {
+            ctx.moveTo(orgX, 0);
+            ctx.lineTo(orgX, canvas.height);
+        }
+        if (orgY > 0 && orgY < canvas.height) {
+            ctx.moveTo(0, orgY);
+            ctx.lineTo(canvas.width, orgY);
+        }
+        ctx.stroke();
+        ctx.strokeStyle = 'black';
+        for (let x = xRange[0]; x <= xRange[1]; x += step) {
+            for (let y = yRange[0]; y <= yRange[1]; y += step) {
+                ctx.beginPath();
+                const screenX = x * scale + orgX;
+                const screenY = - y * scale + orgY;
+                const fieldValue = fn(x, y);
+                if (fieldValue) {
+                    if (fieldValue.angle) {
+                        ctx.moveTo(screenX, screenY);
+                        const amplitude = fieldValue.amplitude || (step * scale / 2);
+                        const angleInRadians = fieldValue.angle * Math.PI / halfCircle;
+                        ctx.lineTo(screenX + amplitude * Math.cos(angleInRadians), screenY - amplitude * Math.sin(angleInRadians));
+                        ctx.stroke();
+                    }
+                    else if (fieldValue.amplitude) {
+                        ctx.ellipse(screenX, screenY, fieldValue.amplitude, fieldValue.amplitude, 0, 0, 2 * Math.PI);
+                        ctx.fill();
+                    }
+                }
+            }
+        }
+    };
 
 document.addEventListener('DOMContentLoaded', e => {
     const map = new Map(mapSize);
     const ship = new Valkyrie();
     const mapEl = document.getElementsByClassName('map')[0];
     const shipImg = document.getElementsByClassName('ship')[0];
-    const overhead = new OverheadMap(mapEl, shipImg, mapScale, map, ship,
+    new OverheadMap(mapEl, shipImg, mapScale, map, ship,
         val => ({
             r: Math.floor(0x68 + 0x80 * val / maxHeight),
             g: Math.floor(0x60 + 0x40 * val / maxHeight),
@@ -494,7 +707,36 @@ document.addEventListener('DOMContentLoaded', e => {
         }));
     map.generate();
     const viewportEl = document.getElementById('viewport');
-    const viewport = new Viewport(viewportEl, viewportWidth, viewportHeight, viewportScale, map, ship);
+    new Viewport(viewportEl, viewportWidth, viewportHeight, viewportScale, map, ship);
+
+    // Tests
+    const testSection = document.getElementById("testSection");
+    const testGraph = graphTester(testSection);
+    const testRange = rangeTester(testSection);
+    const testField = fieldTester(testSection);
+    const runTestsButton = document.getElementById("runTests");
+    runTestsButton.addEventListener("click", () => {
+        testSection.innerHTML = "";
+        testGraph(sin, [-1440, 1440], [-1 << bitsBetweenTops, 1 << bitsBetweenTops], 1/4, 50 / (1 << bitsBetweenTops));
+        testGraph(cos, [-1440, 1440], [-1 << bitsBetweenTops, 1 << bitsBetweenTops], 1/4, 50 / (1 << bitsBetweenTops));
+        testGraph(tan, [-2880, 2880], [-1500, 1500], 1/8, 1/10);
+        testGraph(x => clamp(x, 10), [-1, 15], [-1, 11], 5, 5, "clamp");
+        testGraph(x => vary(x, 5, 40), [-10, 60], [-1, 56], 1, 1, "vary");
+        testGraph(x => mod(x, 10), [-25, 25], [-1, 11], 5, 5, "mod");
+        testRange(
+            x => findIndexOf(x, [3, 7, 15, 15, 16, 18]),
+            [...Array(20).keys()],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 5, 5, 5, 5],
+            "findIndexOf");
+        testField(
+            (x, y) => ({angle: angleFromCoordinates(x, y)}),
+            [-10, 10], [-10, 10], 10, 1,
+            "angleFromCoordinates");
+        testField(
+            (x, y) => ({amplitude: Math.sqrt(x * x + y * y) / 5}),
+            [-10, 10], [-10, 10], 10, 1,
+            "distanceFromCenter");
+    });
 
     // To make the game smoother, we prefer dropped frames to uneven timing -> setInterval, not setTimeout
     setInterval(gameLoop, tick);
