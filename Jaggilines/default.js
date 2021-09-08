8eac7fca9cf71944320a81eaab268c30c7edb856
Bertrand Le roy <bertrandleroy@gmail.com>
2021-09-07 23:51:55 -0700

    Add controls (unwired for now)

--- Jaggilines/default.js
+++ Jaggilines/default.js
@@ -680,10 +680,14 @@ class Valkyrie {
     }
 }
 
+/** A compass showing what direction the ship is headed. */
 class Compass {
     #el;
     #elWidth;
 
+    /** Constructs a compass targeted at an element and for a ship.
+     * @param {HTMLElement} el - The element for the compass.
+     * @param {Valkyrie} ship - The ship. */
     constructor(el, ship) {
         this.#el = el;
         this.#elWidth = el.scrollWidth / 5;
@@ -694,6 +698,7 @@ class Compass {
         });
     }
 
+    /** Updates the compass from the current state of the ship. */
     update() {
         const offset = this.#elWidth * (mod((fullCircle - this.ship.heading) / quarterCircle - 3, 4) + 0.5);
         this.#el.style.left = -offset + 'px';
